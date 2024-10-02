c28bc19b29f2b60ce044e57a8a69341710d29c59
Alejo <alejojpa2@gmail.com>
2024-10-02 05:29:35 -0400

    change handler and updating input

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
+const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    feedback: ''
+  });
+
+const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({...formData,[name]: value});
+};
+
 const FeedbackForm = () => {
 
   return (
@@ -16,15 +27,21 @@ const FeedbackForm = () => {
               type="text"
               name="name"
               placeholder="Your Name"
+              value={formData.name}
+              onChange={handleChange}
             />
             <input
               type="email"
               name="email"
               placeholder="Your Email"
+              value={formData.email}
+              onChange={handleChange}
             />
             <textarea
               name="feedback"
               placeholder="Your Feedback"
+              value={formData.feedback}
+              onChange={handleChange}
             ></textarea>
             <button type="submit">Submit Feedback</button>
         
