c9a0afa222befa7fa46b8e0e5ae1d737efdb8391
Alejo <alejojpa2@gmail.com>
2024-10-15 13:29:24 +0200

    First Commit, static from template

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './FeedbackForm.css'; // Import CSS for styling
 
-const FeedbackForm = () => {
 
+const FeedbackForm = () => {
+/* Input components: 1) input box for username, 2) input box for user email ID, 3) input box for user feedback */
   return (
     <>
     <nav>
@@ -11,7 +12,25 @@ const FeedbackForm = () => {
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+        <input 
+          type="text"
+          name="name" 
+          placeholder="Your Name">
+        </input>
+
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="Your Email">
+        </input>
+
+        <textarea 
+          name="feedback" 
+          placeholder="Your Feedback">
+        </textarea>
+
+        <button type="submit">Submit Feedback</button>
+   
       </form>
     </>
   );
